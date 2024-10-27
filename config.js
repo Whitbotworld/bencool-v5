7374efd24e3cbe27dec08c89a7eaa74917b137e4
Whitbotworld <w07807263@gmail.com>
2024-10-27 07:44:11 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,14 +7,14 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['16362439088', 'kinfux digital', true],
-  ['254110377776', 'kinflux digital', true], 
-  ['556199740980'] 
+  ['243105324632', 'rampage', true],
+  ['254105324632', 'rampage', true], 
+  ['254105324632'] 
 ] //Number of owners
 
-global.mods = ['16362439088','254110377776','556199740980'] 
-global.prems = ['22589747147', '254110377776', '556199740980']
-global.allowed = ['556199740980', '254110377776', '13374230362']
+global.mods = ['254105324632'] 
+global.prems = ['254105324632']
+global.allowed = ['254105324632']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
@@ -57,10 +57,10 @@ global.dygp = 'https://chat.whatsapp.com/BLjStbNJvG2H7Z3hlDmv28'
 global.fgsc = 'https://github.com/diggilly/kinflux-bot' 
 global.fgyt = 'https://chat.whatsapp.com/BLjStbNJvG2H7Z3hlDmv28'
 global.fgpyp = ''
-global.fglog = 'https://raw.githubusercontent.com/diggilly/kinflux-bot/main/Guru.jpg' 
+global.fglog = '' 
 
 
-global.wait = '*⌛ _kinflux bot loading..._*\n*▰▰▰▱▱▱▱▱*'
+global.wait = '⌛ _kinflux bot loading ▰▰▰▱▱▱▱▱*'
 global.rwait = '⌛'
 global.dmoji = '🤭'
 global.done = '✅'
